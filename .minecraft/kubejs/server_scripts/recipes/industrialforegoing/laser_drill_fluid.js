672ef193b900ee6907c571ccc77bd76dd0df7e6b
FalAut <2907281799@qq.com>
2024-03-06 14:27:47 +0800

    len

--- .minecraft/kubejs/server_scripts/recipes/industrialforegoing/laser_drill_fluid.js
+++ .minecraft/kubejs/server_scripts/recipes/industrialforegoing/laser_drill_fluid.js
@@ -13,7 +13,7 @@ onEvent("recipes", (event) => {
       ],
       pointer: 0,
       catalyst: {
-        item: "kubejs:air",
+        item: "industrialforegoing:laser_lens6",
       },
       entity: "botania:pink_wither",
       type: "industrialforegoing:laser_drill_fluid",
@@ -34,7 +34,7 @@ onEvent("recipes", (event) => {
       ],
       pointer: 0,
       catalyst: {
-        item: "kubejs:air",
+        item: "industrialforegoing:laser_lens15",
       },
       entity: "wither",
       type: "industrialforegoing:laser_drill_fluid",
