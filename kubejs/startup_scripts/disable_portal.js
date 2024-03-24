22f6f546e6b7e59bd3486d4efe265ba31ece76a7
FalAut <143469089+FalAut@users.noreply.github.com>
2024-03-24 17:36:54 +0800

    v1.6.3

--- kubejs/startup_scripts/disable_portal.js
+++ kubejs/startup_scripts/disable_portal.js
@@ -7,7 +7,7 @@ const CustomPortalBuilder = java("net.kyrptonaught.customportalapi.api.CustomPor
 onEvent("postinit", (event) => {
   CustomPortalBuilder.beginPortal()
     ["frameBlock(net.minecraft.resources.ResourceLocation)"](
-      new ResourceLocation("kubejs", "nether_portal_frame_filled")
+      new ResourceLocation("kubejs", "nether_portal_frame_filled") //XD
     )
     .destDimID(new ResourceLocation("minecraft", "the_nether"))
     .tintColor(61, 61, 61)
--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -9,10 +9,7 @@ onEvent("item.registry", (event) => {
   event.create("ghast_eye")
   event.create("wither_skeleton_eye")
   event.create("nether_eye")
-  event
-    .create("incomplete_queens_slime_ingot", "create:sequenced_assembly")
-    .textureJson({ layer0: "tconstruct:item/materials/queens_slime_ingot" })
-    .color(0, 0xb5b5b5)
+  event.create("incomplete_queens_slime_ingot", "create:sequenced_assembly").textureJson({ layer0: "tconstruct:item/materials/queens_slime_ingot" }).color(0, 0xb5b5b5)
   event
     .create("tainted_blend", "create:sequenced_assembly")
     .textureJson({
@@ -23,10 +20,7 @@ onEvent("item.registry", (event) => {
     .color(0, 0x8b6914)
     .color(1, 0x8b6914)
     .color(2, 0x8b6914)
-  event
-    .create("ferro_coke_blend")
-    .textureJson({ layer0: "architects_palette:item/nether_brass_blend" })
-    .color(0, 0x808080)
+  event.create("ferro_coke_blend").textureJson({ layer0: "architects_palette:item/nether_brass_blend" }).color(0, 0x808080)
   event.create("enriched_dragon_breath").texture("mekanism:item/enriched_tin").color(0, 0xffc0cb)
   event.create("enriched_unobtanium").texture("mekanism:item/enriched_refined_obsidian").color(0, 0xffc0cb)
   event.create("mold_rf_coil")
@@ -34,23 +28,15 @@ onEvent("item.registry", (event) => {
   event.create("incomplete_rf_coil", "create:sequenced_assembly")
   event.create("incomplete_redstone_servo", "create:sequenced_assembly")
   event.create("mana_emerald").textureJson({ layer0: "minecraft:item/emerald" }).color(0, 0x00bfff).glow(true)
-  event
-    .create("incomplete_creative_mana_tablet", "create:sequenced_assembly")
-    .textureJson({ layer0: "botania:item/mana_tablet", layer1: "botania:item/mana_tablet_overlay" })
-    .color(1, 0x808080)
+  event.create("incomplete_creative_mana_tablet", "create:sequenced_assembly").textureJson({ layer0: "botania:item/mana_tablet", layer1: "botania:item/mana_tablet_overlay" }).color(1, 0x808080)
 })
 
 onEvent("block.registry", (event) => {
   event.create("undef1ned_block").material("amethyst").displayName("Block of Creation").hardness(0.1)
   event.create("earth_core").material("amethyst").hardness(0.1)
+  //必须在这里注册2个下界传送门框架 因为 → startup_scripts/disable_portal.js #10
   event.create("nether_portal_frame").material("stone").lightLevel(1).box(0, 0, 0, 16, 13, 16).defaultCutout()
-  event
-    .create("nether_portal_frame_filled")
-    .material("stone")
-    .lightLevel(1)
-    .box(0, 0, 0, 16, 13, 16)
-    .box(4, 12, 4, 12, 16, 12)
-    .defaultCutout()
+  event.create("nether_portal_frame_filled").material("stone").lightLevel(1).box(0, 0, 0, 16, 13, 16).box(4, 12, 4, 12, 16, 12).defaultCutout()
   event
     .create("smoked_iron_block")
     .material("metal")
@@ -63,20 +49,12 @@ onEvent("block.registry", (event) => {
 })
 
 onEvent("fluid.registry", (event) => {
-  let undef1ned_fluid = event
-    .create("undef1ned_fluid")
-    .displayName("Fluid of Creation")
-    .stillTexture("kubejs:fluid/undef1ned_fluid")
-    .flowingTexture("kubejs:fluid/undef1ned_fluid")
+  let undef1ned_fluid = event.create("undef1ned_fluid").displayName("Fluid of Creation").stillTexture("kubejs:fluid/undef1ned_fluid").flowingTexture("kubejs:fluid/undef1ned_fluid")
   let AAttributes = undef1ned_fluid.createAttributes()
   AAttributes.tickDelay(1)
   undef1ned_fluid.attributes = AAttributes
 
-  event
-    .create("ichor_slime")
-    .stillTexture("tconstruct:block/fluid/ichor/still")
-    .flowingTexture("tconstruct:block/fluid/ichor/flowing")
-    .bucketColor(0xff7f50)
+  event.create("ichor_slime").stillTexture("tconstruct:block/fluid/ichor/still").flowingTexture("tconstruct:block/fluid/ichor/flowing").bucketColor(0xff7f50)
   event.create("molten_andesite").thickTexture(0xdcdcdc)
   event.create("molten_andesite_alloy").thickTexture(0xbebebe)
   event.create("mana").thinTexture(0x87ceeb).noBucket()
