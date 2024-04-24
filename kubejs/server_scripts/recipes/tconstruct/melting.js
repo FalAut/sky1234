d9bb7d623d450dd94cce5ab7eef32163ec3221e5
FalAut <143469089+FalAut@users.noreply.github.com>
2024-04-24 13:18:17 +0800

    匠魂熔炼配方兼容

--- kubejs/server_scripts/recipes/tconstruct/melting.js
+++ kubejs/server_scripts/recipes/tconstruct/melting.js
@@ -1,26 +1,42 @@
-onEvent("recipes", (event) => {
-  const id_prefix = "kubejs:recipes/tconstruct/melting/"
-  const recipes = [
-    {
-      input: "tconstruct:ichor_slime_ball",
-      output: "kubejs:ichor_slime",
-      amount: 250,
-      temperature: 50,
-      time: 31,
-      id: `${id_prefix}ichor_slime`,
-    },
-    {
-      input: "minecraft:andesite",
-      output: "kubejs:molten_andesite",
-      amount: 100,
-      temperature: 1000,
-      time: 48,
-      id: `${id_prefix}molten_andesite`,
-    },
-  ]
+onEvent('recipes', (event) => {
+    const id_prefix = 'kubejs:recipes/tconstruct/melting/'
+    const recipes = [
+        {
+            input: 'tconstruct:ichor_slime_ball',
+            output: 'kubejs:ichor_slime',
+            amount: 250,
+            temperature: 50,
+            time: 31,
+            id: `${id_prefix}ichor_slime1`,
+        },
+        {
+            input: 'tconstruct:ichor_congealed_slime',
+            output: 'kubejs:ichor_slime',
+            amount: 1000,
+            temperature: 50,
+            time: 31,
+            id: `${id_prefix}ichor_slime2`,
+        },
+        {
+            input: 'minecraft:andesite',
+            output: 'kubejs:molten_andesite',
+            amount: 100,
+            temperature: 1000,
+            time: 48,
+            id: `${id_prefix}molten_andesite`,
+        },
+        {
+            input: 'kubejs:congealed_magma_cream',
+            output: 'tconstruct:magma',
+            amount: 1000,
+            temperature: 300,
+            time: 48,
+            id: `${id_prefix}magma`,
+        },
+    ]
 
-  recipes.forEach((recipe) => {
-    ;(recipe.type = "tconstruct:melting"), (recipe.ingredient = { item: recipe.input })
-    ;(recipe.result = { fluid: recipe.output, amount: recipe.amount }), event.custom(recipe).id(recipe.id)
-  })
+    recipes.forEach((recipe) => {
+        ;(recipe.type = 'tconstruct:melting'), (recipe.ingredient = { item: recipe.input })
+        ;(recipe.result = { fluid: recipe.output, amount: recipe.amount }), event.custom(recipe).id(recipe.id)
+    })
 })
